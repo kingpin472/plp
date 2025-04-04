713f84f2d41a5ee19f4221a416ad47b4dd752010
joshua <105699037+kingpin472@users.noreply.github.com>
2025-04-04 21:11:07 +0300

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,16 @@
+num1=float(input("enter first number: "))
+op=input("Enter operator: ")
+num2=float(input("enter second number number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op =="-" :
+    print(num1 - num2)
+elif op=="/":
+    print(num1/num2)
+elif op=="*":
+    print(num1*num2)     
+else:
+    print("Invalid input")      
+
+
